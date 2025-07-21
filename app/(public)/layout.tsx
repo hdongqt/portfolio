fc0308568fdd694f6105e21fe551af678071b3e1
hdongqt <hdongqt@gmail.com>
2025-07-21 13:28:46 +0700

    feat(seo): add meta gg

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,5 +1,6 @@
 import Header from '@/app/(public)/header'
 import FluidCursor from '@/components/layout/fluid-cursor'
+import Head from 'next/head'
 
 export default async function Layout({
   children
@@ -9,6 +10,9 @@ export default async function Layout({
   return (
     <div className='flex min-h-screen w-full flex-col relative'>
       <Header />
+      <Head>
+        <meta name='google-site-verification' content='8AMUM_GmhT7uAQ0cJcYiptoDGBL4Y9JNXWZbPxuzC-8' />
+      </Head>
       <main className='z-20'>{children}</main>
       <footer className='text-sm md:text-base flex items-center justify-center font-semibold text-zinc-600 w-full py-3'>
         © 2025 Dong Dev. All rights reserved.
